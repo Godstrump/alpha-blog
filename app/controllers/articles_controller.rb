51f0dac78952bdf49cba96b23af07b7a0a1a5b4c
Godstrump <GGodstrump@gmail.com>
2021-10-11 17:19:15 +0100

    Style form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to articles_path(@article)
+      redirect_to @article
     else
       # or :new
       render 'new'
